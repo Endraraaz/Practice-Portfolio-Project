8270ff60015f43f0b271a27496a9252c466fbb9c
endraraaz <endraraaz@gmail.com>
2018-10-14 17:41:34 +0545

    jobs displayed in homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title=models.TextField()
+    image=models.ImageField(upload_to='images/')
+    summary=models.CharField(max_length=200)
+    pub_date=models.DateTimeField()
+
+    
